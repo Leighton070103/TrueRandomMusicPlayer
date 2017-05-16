72e1b96234eb6aa5c59e88a023f79fd0fad24171
bridget_103 <might_103@yeah.net>
2017-05-16 14:53:07 +0800

    Change the listView into recyclerview, created the adapter and the corresponding activity

---
+++ app/src/main/java/net/classicgarage/truerandommusicplayer/adapter/SongAdapter.java
@@ -0,0 +1,62 @@
+package net.classicgarage.truerandommusicplayer.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.classicgarage.truerandommusicplayer.R;
+import net.classicgarage.truerandommusicplayer.model.SongItem;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
+/**
+ * Created by Tong on 2017/5/16.
+ */
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
+
+    private ArrayList<SongItem> mSongs;
+    private View mItemView;
+    private Context mContext;
+
+    public SongAdapter(Context context, ArrayList<SongItem> songs){
+        mSongs = songs;
+        mContext = context;
+    }
+    @Override
+    public SongAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        mItemView = LayoutInflater.from( parent.getContext() ).inflate(R.layout.song_item,
+                parent, false);
+        return new ViewHolder(mItemView);
+    }
+
+    @Override
+    public void onBindViewHolder(SongAdapter.ViewHolder holder, int position) {
+        SongItem song = mSongs.get(position);
+        holder.mSongTitleTv.setText( song.getTitle() );
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSongs.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mSongTitleTv;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mSongTitleTv = (TextView) itemView.findViewById(R.id.item_song_title_tv);
+        }
+    }
+}
