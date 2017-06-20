a5c93b38f3afed263b381dd6386b8640619a7a51
bridget_103 <might_103@yeah.net>
2017-06-21 03:49:47 +0800

    Start work of the lockscreen

---
+++ app/src/main/java/net/classicgarage/truerandommusicplayer/activity/LockScreenActivity.java
@@ -0,0 +1,15 @@
+package net.classicgarage.truerandommusicplayer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.classicgarage.truerandommusicplayer.R;
+
+public class LockScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+    }
+}
